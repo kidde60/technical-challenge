c5e0bdac3cc61347036826c18469bf1d5f65230a
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-21 09:16:08 +0300

    Creare book card

---
+++ src/BookList/BookCard.jsx
@@ -0,0 +1,35 @@
+// import React from "react";
+
+// const BookCard = ({ data }) => {
+//   console.log(data);
+//   //   isbn	string
+//   // title	string
+//   // subTitle	string
+//   // author	string
+//   // publish_date	string($date-time)
+//   // publisher	string
+//   // pages	number
+//   // description	string
+//   // website
+//   return (
+//     <div>
+//       <h1>Books</h1>
+//       {/* <div className="container">
+//         <div>
+//           <h1>{data.isbn}</h1>
+//           <h1>{data.title}</h1>
+//           <h2>{data.subTitle}</h2>
+//           <h3>{data.author}</h3>
+//           <h4>{data.publish_date}</h4>
+//           <h3>{data.publisher}</h3>
+//           <h3>{data.pages}</h3>
+//           <p>{data.description}</p>
+//           <p>{data.website}</p>
+//         </div>
+//       </div>
+//       <h1> {data.title}</h1>
+//     </div> */}
+//   );
+// };
+
+// export default BookCard;
