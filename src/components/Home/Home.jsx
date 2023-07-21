1c29bd06add1c24ad62ea1f2ecc6fb45476048d8
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-21 09:18:30 +0300

    Add home opage

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addBooks } from "../../Features/BookSlice";
+import BookList from "../../BookList/BookList";
+
+export const Home = () => {
+  // const url = 'https://bookstore.toolsqa.com/BookStore/v1/Books';
+  const url = "https://bookstore.toolsqa.com/BookStore/v1/Books";
+  const dispatch = useDispatch();
+
+  const fetchBooks = async () => {
+    const response = await axios.get(url).catch((err) => {
+      console.log("Error :", err);
+    });
+    dispatch(addBooks(response.data));
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  return (
+    <div>
+      <BookList />
+    </div>
+  );
+};
