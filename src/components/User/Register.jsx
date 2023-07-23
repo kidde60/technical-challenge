f299df5013f93d7c0c48a9cf75b35e28eb0afb41
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-23 10:51:11 +0300

    impliment register

--- src/components/User/Register.jsx
+++ src/components/User/Register.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [userName, setUserName] = useState("");
@@ -38,23 +39,44 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="form">
+      <form
+        onSubmit={handleSubmit}
+        className="container m-8 gap-4 flex flex-col  p-10"
+      >
         <input
           type="text"
           onChange={(e) => setUserName(e.target.value)}
           value={userName}
           placeholder="User Name"
+          className="bg-[#e5e7eb] py-2 px-4 text-base border-solid border-2 border-gray-400"
         />
         <input
           type="password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           placeholder="Password"
+          className="bg-[#e5e7eb] py-2 px-4 my-3 text-base border-solid border-2 border-gray-400"
         />
-        <button type="submit" onSubmit={handleSubmit}>
-          sign up
-        </button>
+        <div className="flex gap-20 mx-4">
+          <button
+            type="submit"
+            className="bg-[#49cc90] px-4 py-2 rounded-full max-w-max"
+            onSubmit={handleSubmit}
+          >
+            Register
+          </button>
+          {/* <button
+            type="submit"
+            onSubmit={handleSubmit}
+            className="bg-[#61affe] px-4 py-2 rounded-full max-w-max"
+          >
+            Login
+          </button> */}
+          <Link to={"/Login"} className="text-blue-500">
+            Login
+          </Link>
+        </div>
       </form>
     </div>
   );
