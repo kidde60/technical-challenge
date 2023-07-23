9bc27200c4c057bd64d451fbe8893948137b8174
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-23 10:50:50 +0300

    Impliment add login

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,22 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
     if (validate) {
       genToken();
-      //   axios
-      //     .post("https://bookstore.toolsqa.com/Account/v1/User", options)
-      //     .then((res) => {
-      //       console.log(res.data);
-      //     })
-      //     .catch((error) => console.log(error));
-      //   console.log("success");
-      // } else {
-      //   console.log("error");
     }
   };
   console.log("name: ", userName);
@@ -31,9 +24,23 @@ const Login = () => {
     const response = await axios.post(url, options).catch((err) => {
       console.log("Error :", err);
     });
-    console.log(response.data.token);
+    console.log(response);
+    if (response.data.status === "Success") {
+      login();
+      navigate("/home");
+    }
   };
-
+  const login = async () => {
+    const url = "https://bookstore.toolsqa.com/Account/v1/Authorized";
+    const options = {
+      userName: userName,
+      password: password,
+    };
+    const result = await axios
+      .post(url, options)
+      .catch((error) => console.log("error: ", error));
+    console.log(result);
+  };
   let isValid = true;
   const validate = () => {
     if (userName === "" || userName === null) {
@@ -48,22 +55,36 @@ const Login = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className="container m-8 gap-4 flex flex-col  p-10"
+      >
         <input
           type="text"
           onChange={(e) => setUserName(e.target.value)}
           value={userName}
           placeholder="User Name"
+          className="bg-[#e5e7eb] py-2 px-4 my-3 text-base border-solid border-2 border-gray-400"
         />
         <input
           type="password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
           placeholder="Password"
+          className="bg-[#e5e7eb] py-2 px-4 text-base border-solid border-2 border-gray-400"
         />
-        <button type="submit" onSubmit={handleSubmit}>
-          sign up
-        </button>
+        <div className="flex gap-20 mx-4">
+          <button
+            type="submit"
+            onSubmit={handleSubmit}
+            className="bg-[#61affe] px-4 py-2 rounded-full max-w-max"
+          >
+            Login
+          </button>
+          <Link to={"/"} className="text-blue-500">
+            Register
+          </Link>
+        </div>
       </form>
     </div>
   );
