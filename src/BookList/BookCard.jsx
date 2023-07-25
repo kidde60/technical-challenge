2fbfde91062360581f20e9755d174f41c7c536a1
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-25 14:19:30 +0300

    create book card

--- src/BookList/BookCard.jsx
+++ src/BookList/BookCard.jsx
@@ -1,35 +1,68 @@
-// import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./bookCard.css";
+const BookCard = () => {
+  const [dataArray, setDataArray] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-// const BookCard = ({ data }) => {
-//   console.log(data);
-//   //   isbn	string
-//   // title	string
-//   // subTitle	string
-//   // author	string
-//   // publish_date	string($date-time)
-//   // publisher	string
-//   // pages	number
-//   // description	string
-//   // website
-//   return (
-//     <div>
-//       <h1>Books</h1>
-//       {/* <div className="container">
-//         <div>
-//           <h1>{data.isbn}</h1>
-//           <h1>{data.title}</h1>
-//           <h2>{data.subTitle}</h2>
-//           <h3>{data.author}</h3>
-//           <h4>{data.publish_date}</h4>
-//           <h3>{data.publisher}</h3>
-//           <h3>{data.pages}</h3>
-//           <p>{data.description}</p>
-//           <p>{data.website}</p>
-//         </div>
-//       </div>
-//       <h1> {data.title}</h1>
-//     </div> */}
-//   );
-// };
+  // Step 2: Fetch the data from the API inside useEffect hook (runs once on component mount)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://bookstore.toolsqa.com/BookStore/v1/Books"
+        );
+        const data = await response.json();
+        setDataArray(data); // Save the data in the state variable
+        setIsLoading(false); // Update isLoading to false once data is fetched
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setIsLoading(false); // Update isLoading to false on error as well
+      }
+    };
 
-// export default BookCard;
+    fetchData();
+  }, []);
+
+  // Step 3: Conditional rendering based on the isLoading state
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  console.log(dataArray.books);
+  // Step 4: Render the data once it's available
+  return (
+    <div className="mx-8">
+      <div className="cardcontainer">
+        <h1>BOOK LIST</h1>
+        <Link to={"/book"} className="text-blue-600">
+          <button
+            type="button"
+            className="bg-[#61affe] px-4 py-2 rounded-full max-w-max"
+          >
+            Add Book
+          </button>
+        </Link>
+      </div>
+      <div className="container gap-8">
+        {dataArray.books.map((data) => (
+          <div
+            key={data.id}
+            className=" container bg-[#e5e7eb] py-2 px-4 my-3 text-base border-solid border-2 border-gray-400"
+          >
+            <h1>Title: {data.title}</h1>
+            <h1>Isbn: {data.isbn}</h1>
+            <h2>SubTitle: {data.subTitle}</h2>
+            <h3>Author: {data.author}</h3>
+            <h4>Published_date: {data.publish_date}</h4>
+            <h3>Publisher: {data.publisher}</h3>
+            <h3>Date: {data.pages}</h3>
+            <p>Description: {data.description}</p>
+            <p>Website: {data.website}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
