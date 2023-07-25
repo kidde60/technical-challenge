4fb5e5f1a47e8a67e389a8422b719222c2c9fccf
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-25 21:13:24 +0300

    Add find book routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar/Navbar";
 import { Footer } from "./components/Footer/Footer";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
+import FindBook from "./components/FindBook/FindBook";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/books/:id" element={<BookDetails />} />
         <Route path="/book" element={<AddBook />} />
+        <Route path="/find" element={<FindBook />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Register />} />
       </Routes>
