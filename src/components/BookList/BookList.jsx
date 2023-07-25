86f94b525d3ef062f751f1a6053c22a554751c1a
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-25 21:10:44 +0300

    Update booklist

---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { getAllBooks } from "../Features/BookSlice";
+import { Link } from "react-router-dom";
+
+// import BookCard from "./BookCard";
+const BookList = () => {
+  const books = useSelector(getAllBooks);
+  // console.log(books);
+
+  // let renderBooks = "";
+  // renderBooks =
+  // books.length !== 0 ? (
+  //   books.map((book, index) => {
+  //     // console.log(book);
+  //     <div>
+  //       <h1>{book.isbn}</h1>
+  //     </div>;
+  //     //     <div className="container">
+
+  //     //       <h1>{data.isbn}</h1>
+  //     //       <h1>{data.title}</h1>
+  //     //       <h2>{data.subTitle}</h2>
+  //     //       <h3>{data.author}</h3>
+  //     //       <h4>{data.publish_date}</h4>
+  //     //       <h3>{data.publisher}</h3>
+  //     //       <h3>{data.pages}</h3>
+  //     //       <p>{data.description}</p>
+  //     //       <p>{data.website}</p>
+  //     //     </div>
+  //     //   </div>
+  //     //   <h1> {data.title}</h1>
+  //     // </div>
+  //   })
+  // ) : (
+  //   <div>
+  //     <h3>{books.error}</h3>
+  //   </div>
+  // );
+  // console.log(renderBooks);
+  return (
+    <>
+      <h1>Books</h1>
+      {(() => {
+        if (books !== undefined) {
+          <h1>array present</h1>;
+          // books.map((book, index) => {
+          //   console.log(book.title);
+          //   return <h1>book.title</h1>;
+          // });
+        } else {
+          console.log("......loading");
+        }
+      })()}
+      <p>hgfgdffjghgk</p>
+      <Link to={"/book"} className="text-blue-600">
+        <button
+          type="button"
+          className="bg-[#61affe] px-4 py-2 rounded-full max-w-max"
+        >
+          Add Book
+        </button>
+      </Link>
+    </>
+  );
+};
+
+export default BookList;
