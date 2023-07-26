b711904999f72ab76fd50c586bfd1c56b22fe73d
kidde60 <kgeorgewilliam60@gmail.com>
2023-07-26 04:25:58 +0300

    Update app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { BookDetails } from "./components/BookDetails/BookDetails";
 import { AddBook } from "./components/AddBook/AddBook";
-import Navbar from "./components/Navbar/Navbar";
-import { Footer } from "./components/Footer/Footer";
 import Login from "./components/User/Login";
 import Register from "./components/User/Register";
 import FindBook from "./components/FindBook/FindBook";
