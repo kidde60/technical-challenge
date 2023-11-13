0b2589c48c955fdcc664f73ccbaa38ed4d4b09eb
GEORGE WILLIAM KIDDE <kgeorgewilliam60@gmail.com>
2023-11-13 21:05:46 +0300

    Update Login.jsx

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
-
 const Login = () => {
   const navigate = useNavigate();
   const [userName, setUserName] = useState("");
